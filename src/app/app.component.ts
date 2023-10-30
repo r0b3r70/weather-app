579c478a6b2912620479aba4eca31dbb74552ed6
Robert <r0b3r70@gmail.com>
2023-10-30 16:32:24 +0100

    cleanup styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'weather-app';
+  title = 'Brand New Weather App 🎉';
 }
+
