61524b5334d9dc607378a9f4cca3c447df3d03ec
Robert <r0b3r70@gmail.com>
2023-11-01 11:49:03 +0100

    linting

--- src/app/state/weather/weather.actions.ts
+++ src/app/state/weather/weather.actions.ts
@@ -1,4 +1,4 @@
-import { createActionGroup, emptyProps, props } from '@ngrx/store';
+import { createActionGroup, props } from '@ngrx/store';
 import {
     GeoLocation,
     CurrentWeatherAPIResponse,
